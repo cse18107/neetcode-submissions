4ea2ef973c9154c326b92239d8c7c6b5f6a8dd6d
Soumodeep Maity <87257845+cse18107@users.noreply.github.com>
2026-07-20 00:39:44 +0530

    Add: counting-bits - submission-0

---
+++ Data Structures & Algorithms/counting-bits/submission-0.js
@@ -0,0 +1,16 @@
+class Solution {
+    /**
+     * @param {number} n
+     * @return {number[]}
+     */
+    countBits(n) {
+
+        const bits = new Array(n + 1).fill(0);
+
+        for (let i = 1; i <= n; i++) {
+            bits[i] = bits[i >> 1] + (i & 1);
+        }
+
+        return bits;
+    }
+}
